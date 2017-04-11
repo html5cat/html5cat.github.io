876cf637dbf84da29c119956f7fbb5b2218ef088
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-04-11 13:58:03 -0700

    Site updated at 2017-04-11 20:58:03 UTC

--- source/doge/run.js
+++
@@ -1,4 +0,0 @@
-var sys = require('sys');
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-exec("./minerd --url=stratum+tcp://doge.cryptovalley.com:3333 --userpass=dybskiy.doge:doge", puts);
